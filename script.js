dd59b23375e7d401272115b8989eb3fadb5d4513
arto14 <93529881+arto14@users.noreply.github.com>
2024-09-30 22:01:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    const messageElement = document.getElementById("message");
+    messageElement.innerHTML = "Tombol Telah Di Klik!";
+    messageElement.style.color = "Green";
+}
